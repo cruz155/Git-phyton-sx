0a500b42dcb97e7ed7dca34196daf85a5a867f2d
cruz155 <matheusinaciocruzsouza22@gmail.com>
2025-08-29 16:53:37 -0300

    Add files via upload

---
+++ exercicio 5.py
@@ -0,0 +1,20 @@
+import os
+os.system("cls")
+
+#ENTRADA DE DADOS
+
+num1 = float(input("Digite sua primeiro numero"))
+num2 = float(input("Digite seu segundo numero"))
+operador = input("Digite seu operador (+ -, *, /,): ")
+
+#ESTRUTURA
+match operador:
+    case "+":
+    case    
+
+            
+    
+
+
+      
+
